636a486ff5e927dc50c5f5ef8420733d9e754f65
paulomantilla04 <ma475671@uaeh.edu.mx>
2025-07-12 22:10:48 -0600

    fix: support client-side routing on Vercel

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import App from "./App";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
-import Invitation from "./pages/Invitation";
 import InvitationTest from "./pages/InvitationTest";
 
 
@@ -20,9 +19,6 @@ export const router = createBrowserRouter([
                         <Dashboard />
                     </ProtectedRoute>
                 )
-            },
-            {
-                path: "/invitation/:id", element: <Invitation />
             },
             {
                 path: "/invite", element: <InvitationTest />
